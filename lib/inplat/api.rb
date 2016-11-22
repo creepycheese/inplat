361bcd8a6a34573c7bd83bc95b9689a2354c8073
Evgeniy Burdaev <inqify@gmail.com>
2016-11-22 16:09:56 +0300

    delete unused classes

--- lib/inplat/api.rb
+++ lib/inplat/api.rb
@@ -1,6 +1,31 @@
 module Inplat
+  class SignedRequest
+    attr_reader :host, :secret, :api_key
+
+    def initialize(params)
+      @host = params.fetch(:host)
+      @request_params = params.fetch(:request_params)
+      @secret = params.fetch(:secret)
+      @api_key = params.fetch(:api_key)
+    end
+
+    def request_body
+      @request_body ||= @request_params.to_json
+    end
+
+    def signed_uri
+      @uri ||= "#{host}/?api_key=#{api_key}&sign=#{sign}"
+    end
+
+    private
+
+    def sign
+      @sign = OpenSSL::HMAC.hexdigest('SHA256', secret, request_body)
+    end
+  end
+
   class Api
-    POST_METHODS = ["form", "init"].freeze
+    POST_METHODS = ["form", "init", "check"].freeze
 
     def initialize(configuration, client)
       @configuration = configuration
@@ -10,11 +35,10 @@ module Inplat
     POST_METHODS.each do |method_name|
       define_method(method_name) do |params|
         params = params.merge(method: method_name)
-        request_body = params.to_json
-        sign = OpenSSL::HMAC.hexdigest('SHA256', configuration.secret, request_body)
+        signed_request = SignedRequest.new(host: configuration.host, request_params: params,
+                                             secret: configuration.secret, api_key: configuration.api_key)
 
-        uri = "#{configuration.host}/?api_key=#{configuration.api_key}&sign=#{sign}"
-        client.post(uri, request_body, {content_type: "application/json"})
+        JSON.parse(client.post(signed_request.signed_uri, signed_request.request_body, {content_type: "application/json"}))
       end
     end
 
